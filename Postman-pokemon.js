5064753d660d88aba86ce8f0e5bfe85c51e79eb7
NathanNavid <naaghajafari@gmail.com>
2023-05-02 22:29:30 -0700

    get request by postman

---
+++ Postman-pokemon.js
@@ -0,0 +1,233 @@
+// 1. https://pokeapi.co/api/v2/berry
+// 2. https://pokeapi.co/api/v2/berry-firmness
+// 3. https://pokeapi.co/api/v2/berry-flavor
+// 4. https://pokeapi.co/api/v2/contest-type
+// 5. https://pokeapi.co/api/v2/contest-effect
+
+// 1.
+{
+    
+    [
+        {
+            "name": "cheri",
+            "url": "https://pokeapi.co/api/v2/berry/1/"
+        },
+        {
+            "name": "chesto",
+            "url": "https://pokeapi.co/api/v2/berry/2/"
+        },
+        {
+            "name": "pecha",
+            "url": "https://pokeapi.co/api/v2/berry/3/"
+        },
+        {
+            "name": "rawst",
+            "url": "https://pokeapi.co/api/v2/berry/4/"
+        },
+        {
+            "name": "aspear",
+            "url": "https://pokeapi.co/api/v2/berry/5/"
+        },
+        {
+            "name": "leppa",
+            "url": "https://pokeapi.co/api/v2/berry/6/"
+        },
+        {
+            "name": "oran",
+            "url": "https://pokeapi.co/api/v2/berry/7/"
+        },
+        {
+            "name": "persim",
+            "url": "https://pokeapi.co/api/v2/berry/8/"
+        },
+        {
+            "name": "lum",
+            "url": "https://pokeapi.co/api/v2/berry/9/"
+        },
+        {
+            "name": "sitrus",
+            "url": "https://pokeapi.co/api/v2/berry/10/"
+        },
+        {
+            "name": "figy",
+            "url": "https://pokeapi.co/api/v2/berry/11/"
+        },
+        {
+            "name": "wiki",
+            "url": "https://pokeapi.co/api/v2/berry/12/"
+        },
+        {
+            "name": "mago",
+            "url": "https://pokeapi.co/api/v2/berry/13/"
+        },
+        {
+            "name": "aguav",
+            "url": "https://pokeapi.co/api/v2/berry/14/"
+        },
+        {
+            "name": "iapapa",
+            "url": "https://pokeapi.co/api/v2/berry/15/"
+        },
+        {
+            "name": "razz",
+            "url": "https://pokeapi.co/api/v2/berry/16/"
+        },
+        {
+            "name": "bluk",
+            "url": "https://pokeapi.co/api/v2/berry/17/"
+        },
+        {
+            "name": "nanab",
+            "url": "https://pokeapi.co/api/v2/berry/18/"
+        },
+        {
+            "name": "wepear",
+            "url": "https://pokeapi.co/api/v2/berry/19/"
+        },
+        {
+            "name": "pinap",
+            "url": "https://pokeapi.co/api/v2/berry/20/"
+        }
+    ]
+}
+
+// 2.
+{
+[
+        {
+            "name": "very-soft",
+            "url": "https://pokeapi.co/api/v2/berry-firmness/1/"
+        },
+        {
+            "name": "soft",
+            "url": "https://pokeapi.co/api/v2/berry-firmness/2/"
+        },
+        {
+            "name": "hard",
+            "url": "https://pokeapi.co/api/v2/berry-firmness/3/"
+        },
+        {
+            "name": "very-hard",
+            "url": "https://pokeapi.co/api/v2/berry-firmness/4/"
+        },
+        {
+            "name": "super-hard",
+            "url": "https://pokeapi.co/api/v2/berry-firmness/5/"
+        }
+    ]
+}
+// 3.
+{
+    [
+        {
+            "name": "spicy",
+            "url": "https://pokeapi.co/api/v2/berry-flavor/1/"
+        },
+        {
+            "name": "dry",
+            "url": "https://pokeapi.co/api/v2/berry-flavor/2/"
+        },
+        {
+            "name": "sweet",
+            "url": "https://pokeapi.co/api/v2/berry-flavor/3/"
+        },
+        {
+            "name": "bitter",
+            "url": "https://pokeapi.co/api/v2/berry-flavor/4/"
+        },
+        {
+            "name": "sour",
+            "url": "https://pokeapi.co/api/v2/berry-flavor/5/"
+        }
+    ]
+}
+// 4.
+{
+    [
+        {
+            "name": "cool",
+            "url": "https://pokeapi.co/api/v2/contest-type/1/"
+        },
+        {
+            "name": "beauty",
+            "url": "https://pokeapi.co/api/v2/contest-type/2/"
+        },
+        {
+            "name": "cute",
+            "url": "https://pokeapi.co/api/v2/contest-type/3/"
+        },
+        {
+            "name": "smart",
+            "url": "https://pokeapi.co/api/v2/contest-type/4/"
+        },
+        {
+            "name": "tough",
+            "url": "https://pokeapi.co/api/v2/contest-type/5/"
+        }
+    ]
+}
+// 5. 
+{
+    [
+        {
+            "url": "https://pokeapi.co/api/v2/contest-effect/1/"
+        },
+        {
+            "url": "https://pokeapi.co/api/v2/contest-effect/2/"
+        },
+        {
+            "url": "https://pokeapi.co/api/v2/contest-effect/3/"
+        },
+        {
+            "url": "https://pokeapi.co/api/v2/contest-effect/4/"
+        },
+        {
+            "url": "https://pokeapi.co/api/v2/contest-effect/5/"
+        },
+        {
+            "url": "https://pokeapi.co/api/v2/contest-effect/6/"
+        },
+        {
+            "url": "https://pokeapi.co/api/v2/contest-effect/7/"
+        },
+        {
+            "url": "https://pokeapi.co/api/v2/contest-effect/8/"
+        },
+        {
+            "url": "https://pokeapi.co/api/v2/contest-effect/9/"
+        },
+        {
+            "url": "https://pokeapi.co/api/v2/contest-effect/10/"
+        },
+        {
+            "url": "https://pokeapi.co/api/v2/contest-effect/11/"
+        },
+        {
+            "url": "https://pokeapi.co/api/v2/contest-effect/12/"
+        },
+        {
+            "url": "https://pokeapi.co/api/v2/contest-effect/13/"
+        },
+        {
+            "url": "https://pokeapi.co/api/v2/contest-effect/14/"
+        },
+        {
+            "url": "https://pokeapi.co/api/v2/contest-effect/15/"
+        },
+        {
+            "url": "https://pokeapi.co/api/v2/contest-effect/16/"
+        },
+        {
+            "url": "https://pokeapi.co/api/v2/contest-effect/17/"
+        },
+        {
+            "url": "https://pokeapi.co/api/v2/contest-effect/18/"
+        },
+        {
+            "url": "https://pokeapi.co/api/v2/contest-effect/19/"
+        },
+        {
+            "url": "https://pokeapi.co/api/v2/contest-effect/20/"
+        }
+    ]
+}
