c60b36202b2dc901e7244751d26a2e0bdd8934cf
NathanNavid <naaghajafari@gmail.com>
2023-05-03 17:02:03 -0700

    using @faker library and reduction the code line

--- Faker-Api/index.js
+++ Faker-Api/index.js
@@ -1,22 +1,23 @@
 const express  = require('express')
-const faker = require('faker')
+const {faker} = require('@faker-js/faker')
 const app = express()
 const port = 8000
 
 
+
 const creatUser = () => ({
-    _id: faker.datatype.uuid(),
+    id: faker.datatype.uuid(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    phoneNumber: faker.phone.phoneNumber(),
     password: faker.internet.password(),
     email: faker.internet.email(),
-    phoneNumber: faker.phone.phoneNumber(),
-    lastName: faker.name.lastName(),
-    firstName: faker.name.firstName(),
 })
 
 console.log(creatUser())
 
 const createCopmay=() =>({
-    _id: faker.datatype.uuid(),
+    id: faker.datatype.uuid(),
     name: faker.company.companyName(),
     address: {
         street: faker.address.streetAddress(),
@@ -29,23 +30,29 @@ const createCopmay=() =>({
 console.log(createCopmay())
 
 app.get("/api/users/new" , (req, res)=> {
-    const newUser = creatUser()
-    res.json(newUser);
+    // const newUser = creatUser()
+    // res.json(newUser);
+    res.json(creatUser())
 })
 
 app.get("/api/companies/new" , (req, res)=> {
-    const newCompany = createCopmay()
-    res.json(newCompany);
+    // const newCompany = createCopmay()
+    // res.json(newCompany);
+    res.json(createCopmay())
 })
 
 app.get("/api/user/company", (req, res) =>{
-    const newUser = creatUser();
-    const newCompany = createCopmay();
-    const response = {
-        user: newUser,
-        company: newCompany,
-    };
-    res.json(response);
+    // const newUser = creatUser();
+    // const newCompany = createCopmay();
+    // const response = {
+    //     user: newUser,
+    //     company: newCompany,
+    // };
+    // res.json(response);
+    res.json({
+        user: creatUser(),
+        company: createCopmay(),
+    })
 })
 
 app.listen(port, () => console.log(`express server running on port ${port}`));
